9972466f9844f56db6a383f2770ec41a0b554d81
Wassim Chegham <github@wassimchegham.com>
2017-11-28 14:12:00 +0100

    Update app.angular.module.ts

--- src/app/app.angular.module.ts
+++ src/app/app.angular.module.ts
@@ -1,10 +1,14 @@
+import { EvaluationsDetailsComponent } from './components/risques/evaluations/evaluations-details/evaluations-details.component';
 import * as angular from 'angular';
 import {NgModule, NgModuleFactoryLoader, SystemJsNgModuleLoader} from '@angular/core';
+import {RouterModule} from '@angular/router';
 import {UpgradeModule, setAngularLib} from '@angular/upgrade/static';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import {UIRouterModule} from '@uirouter/angular';
+import { Visualizer } from '@uirouter/visualizer';
+import {UIRouter, UIRouterModule} from '@uirouter/angular';
+import {trace} from '@uirouter/core';
 import {UIRouterUpgradeModule} from '@uirouter/angular-hybrid';
 
 // Modules AngularJS
@@ -12,8 +16,26 @@ import {JenkinsApp} from './app.angularjs.module';
 
 // Module Angular 5
 import {RisquesModule} from './components/risques/risques.module';
+import { StateService } from '@uirouter/angular-hybrid/node_modules/@uirouter/angular';
+
 
 setAngularLib(angular);
+
+trace.enable(1);
+
+export function getJenkinsService($injector) {
+  return $injector.get('jenkins');
+}
+
+export function configRouter(router: UIRouter) {
+  router.plugin(Visualizer);
+}
+
+// export const states = [{
+//   name: '**',
+//   url: '/',
+//   loadChildren: './components/risques/risques.module#RisquesModule',
+// }];
 
 // The main NgModule for the Angular portion of the hybrid app
 @NgModule({
@@ -25,23 +47,16 @@ setAngularLib(angular);
         UIRouterUpgradeModule,
         // Provides the @uirouter/angular directives
         UIRouterModule,
-        NgbModule.forRoot(),
-
         // Modules GAMMA
         RisquesModule,
+        // UIRouterModule.forChild({states, config: configRouter}),
     ],
-    declarations: [],
-    entryComponents: [],
     providers: [
         // Provide the SystemJsNgModuleLoader when using Angular lazy loading
         {provide: NgModuleFactoryLoader, useClass: SystemJsNgModuleLoader},
     ]
 })
 export class JenkinsAngularApp {
-    constructor(private upgrade: UpgradeModule) {
-    }
-
     ngDoBootstrap() {
-        this.upgrade.bootstrap(document.body, [JenkinsApp.name], {strictDi: true});
     }
 }
